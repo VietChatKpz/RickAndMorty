f6d1887bac1c8ce5d1012a8c911665f972714b08
VietChatKpz <>
2023-01-30 14:58:08 +0700

    Create API

--- RickAndMorty/APIClient/RMService.swift
+++ RickAndMorty/APIClient/RMService.swift
@@ -14,7 +14,11 @@ final class RMService {
         
     }
     
-    public func execute(_ request: RMRequest, completion: @escaping () -> Void) {
+    public func execute<T: Codable>(
+        _ request: RMRequest,
+        expecting type: T.Type,
+        completion: @escaping (Result<T, Error>) -> Void
+    ) {
         
     }
 }
---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Đình Việt on 30/01/2023.
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
