3a2bac571df8142a7c9133da769abbdba69e668f
VietChatKpz <>
2023-02-01 10:39:17 +0700

    Character Detail View

--- RickAndMorty/Controllers/Other/RMCharacterDetailViewController.swift
+++ RickAndMorty/Controllers/Other/RMCharacterDetailViewController.swift
@@ -8,9 +8,11 @@
 import UIKit
 
 final class RMCharacterDetailViewController: UIViewController {
-
+    
     private let viewModel: RMCharacterDetailViewViewModel
-
+    
+    private let datailView = RMCharacterDetailView()
+    
     init(viewModel: RMCharacterDetailViewViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -24,5 +26,26 @@ final class RMCharacterDetailViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = viewModel.title
+        view.addSubview(datailView)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .action,
+            target: self,
+            action: #selector(didTapShare))
+        addConstraints()
+        viewModel.fetchCharacterInfo()
+    }
+    
+    @objc
+    private func didTapShare() {
+        
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            datailView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            datailView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            datailView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            datailView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
