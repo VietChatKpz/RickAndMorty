3e87a730be2dd3998aefd9cb92f8c16a2d082c85
VietChatKpz <dinhviet311020@gmail.com>
2023-02-01 15:25:09 +0700

    Compositional Layout

--- RickAndMorty/Controllers/Other/RMCharacterDetailViewController.swift
+++ RickAndMorty/Controllers/Other/RMCharacterDetailViewController.swift
@@ -11,10 +11,11 @@ final class RMCharacterDetailViewController: UIViewController {
     
     private let viewModel: RMCharacterDetailViewViewModel
     
-    private let datailView = RMCharacterDetailView()
+    private let datailView: RMCharacterDetailView
     
     init(viewModel: RMCharacterDetailViewViewModel) {
         self.viewModel = viewModel
+        self.datailView = RMCharacterDetailView(frame: .zero, viewModel: viewModel)
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -33,6 +34,9 @@ final class RMCharacterDetailViewController: UIViewController {
             action: #selector(didTapShare))
         addConstraints()
         viewModel.fetchCharacterInfo()
+        
+        datailView.collectionView?.delegate = self
+        datailView.collectionView?.dataSource = self
     }
     
     @objc
@@ -47,5 +51,27 @@ final class RMCharacterDetailViewController: UIViewController {
             datailView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
             datailView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
+    }
+}
+
+extension RMCharacterDetailViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return viewModel.sections.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        if indexPath.section == 0 {
+            cell.backgroundColor = .systemPink
+        }else if indexPath.section == 1{
+            cell.backgroundColor = .systemGreen
+        }else{
+            cell.backgroundColor = .systemBlue
+        }
+        return cell
     }
 }
